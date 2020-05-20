247ca9adab167cc6b90f7aef8191072cc00e1b48
AtsukiTak <takatomgoo@gmail.com>
2020-05-21 01:33:06 +0900

    Postgres::with_builderの追加。newの修正

--- rego-infra/src/postgres.rs
+++ rego-infra/src/postgres.rs
@@ -1,20 +1,30 @@
 use diesel::{
     pg::PgConnection,
-    r2d2::{ConnectionManager, Pool as PgPool, PooledConnection},
+    r2d2::{Builder, ConnectionManager, Pool, PooledConnection},
 };
 
 #[derive(Clone)]
 pub struct Postgres {
-    pool: PgPool<ConnectionManager<PgConnection>>,
+    pool: Pool<Manager>,
 }
 
-pub type Connection = PooledConnection<ConnectionManager<PgConnection>>;
+pub type Manager = ConnectionManager<PgConnection>;
+pub type Connection = PooledConnection<Manager>;
 
 impl Postgres {
-    pub fn new(url: impl Into<String>) -> Postgres {
+    pub fn new(url: impl Into<String>) -> Result<Postgres, diesel::r2d2::PoolError> {
         let manager = ConnectionManager::<PgConnection>::new(url);
-        let pool = PgPool::new(manager).unwrap();
-        Postgres { pool }
+        let pool = Pool::new(manager)?;
+        Ok(Postgres { pool })
+    }
+
+    pub fn with_builder(
+        url: impl Into<String>,
+        build_func: impl FnOnce(Builder<Manager>) -> Builder<Manager>,
+    ) -> Result<Postgres, diesel::r2d2::PoolError> {
+        let manager = ConnectionManager::<PgConnection>::new(url);
+        let pool = build_func(Pool::builder()).build(manager)?;
+        Ok(Postgres { pool })
     }
 
     pub async fn with_conn<T, F>(&self, func: F) -> anyhow::Result<T>
