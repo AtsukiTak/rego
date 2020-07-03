3b88b890cbc731814125299b62c709cdcf652136
AtsukiTak <takatomgoo@gmail.com>
2020-07-03 12:48:35 +0900

    add rego-err crate

---
+++ rego-err/src/lib.rs
@@ -0,0 +1,53 @@
+use std::borrow::Cow;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    /// Error representing an **input** is at fault.
+    #[error("violate domain invariance rule - {msg}")]
+    BadInput { msg: Cow<'static, str> },
+
+    /// Authentication or Authorization is failed.
+    #[error("auth failed")]
+    AuthFailed,
+
+    /// Some resources are not found.
+    #[error("{resource} is not found")]
+    NotFound { resource: &'static str },
+
+    /// Some resources are conflict.
+    #[error("{resource} is conflict")]
+    Conflict { resource: &'static str },
+
+    /// Error representing an **internal** is at fault.
+    #[error(transparent)]
+    Internal(#[from] anyhow::Error),
+}
+
+impl Error {
+    pub fn bad_input<S>(s: S) -> Self
+    where
+        Cow<'static, str>: From<S>,
+    {
+        Error::BadInput { msg: s.into() }
+    }
+
+    pub fn auth_failed() -> Self {
+        Error::AuthFailed
+    }
+
+    pub fn not_found(resource: &'static str) -> Self {
+        Error::NotFound { resource }
+    }
+
+    pub fn conflict(resource: &'static str) -> Self {
+        Error::Conflict { resource }
+    }
+
+    pub fn internal<E>(e: E) -> Self
+    where
+        E: std::error::Error + Send + Sync + 'static,
+    {
+        Error::Internal(anyhow::Error::from(e))
+    }
+}
--- rego/src/lib.rs
+++ rego/src/lib.rs
@@ -6,3 +6,8 @@ pub use rego_infra as infra;
 
 #[cfg(feature = "rego-domain")]
 pub use rego_domain as domain;
+
+#[cfg(feature = "rego-err")]
+pub use rego_err as error;
+
+pub use error::Error;
